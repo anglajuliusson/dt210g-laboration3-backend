668a48dd96e77088acef542a8b0c45c10ac6f019
Ängla Juliusson <angla02@icloud.com>
2026-02-17 11:58:23 +0100

    Lägg till /admin/me-route för att hämta aktuell användare

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,4 +1,4 @@
-import { addAdmin, deleteAdminById, getAllAdmins, getAdminById, updateAdmin } from './admin.js'; // Importerar controller-funktionerna från admin.js
+import { addAdmin, deleteAdminById, getAllAdmins, getAdminById, updateAdmin, getMe } from './admin.js'; // Importerar controller-funktionerna från admin.js
 
 export async function routes(fastify) {
     // Registrerar GET-route och kopplar till controller-funktionen
@@ -7,4 +7,5 @@ export async function routes(fastify) {
     fastify.post('/admin', addAdmin); // Route för att lägga till ny användare
     fastify.put('/admin/:id', updateAdmin); // Route för att uppdatera en användare med specifikt id
     fastify.delete('/admin/:id', deleteAdminById); // Route för att radera en användare med specifikt id
+    fastify.get('/admin/me', getMe); // ny endpoint
 };
